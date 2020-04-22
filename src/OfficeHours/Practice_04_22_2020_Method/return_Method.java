ed7e82fe124b9f1c080033cfd019ed077b406497
Baha Meral <bahameral@gmail.com>
2020-04-22 12:50:51 -0700

    commit

--- src/OfficeHours/Practice_04_22_2020_Method/return_Method.java
+++ src/OfficeHours/Practice_04_22_2020_Method/return_Method.java
@@ -5,6 +5,10 @@ public class return_Method {
 
      int result = sum2(31,43);
         System.out.println(result);
+
+        String result1 = reverse("baha");
+        System.out.println(result1);
+
     }
 
     public static void sum(int a, int b){
@@ -14,5 +18,11 @@ public class return_Method {
 
         return a+b ;
     }
-
+    public static String reverse(String str){
+        String reuslt = "";
+        for(int i = str.length()-1; i>=0; i--){
+            reuslt += str.charAt(i);
+        }
+        return reuslt;
+    }
 }
