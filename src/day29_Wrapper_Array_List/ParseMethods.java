c5646c4853d37dab3467ddd1dae9edf86ded6b47
bhmeral <bahameral@gmail.com>
2020-04-27 08:50:45 -0700

    Day29Class

---
+++ src/day29_Wrapper_Array_List/ParseMethods.java
@@ -0,0 +1,53 @@
+package day29_Wrapper_Array_List;
+
+public class ParseMethods {
+    public static void main(String[] args) {
+        String str = "123";
+
+        int a1 = Integer.parseInt(str);  //123
+
+        System.out.println(str + 1); // text
+        //        "123" + 1   ==> "1231"
+
+        System.out.println(a1 + 1);  // number
+        //         123 + 1 ==> 124
+
+
+        byte b1 = Byte.parseByte(str);    // byte = 123
+
+        System.out.println(b1 + 5);
+
+        Integer I1 = (int) Byte.parseByte(str);  // AutoBoxing
+
+        // Integer  = (int)byte;
+        // Integer = int;
+
+        String str2 = "10.5";
+
+        float f1 = Float.parseFloat(str2);
+        System.out.println(f1+1);
+
+        Double.parseDouble(str2); // Double 10.5
+
+        double f2 = Double.parseDouble(str2);
+        System.out.println(f2+1);
+
+        String str3 = "3147483647";
+
+        long num1 = Long.parseLong(str3);
+        System.out.println(num1);
+
+        Long num2 = Long.parseLong(str3);
+        System.out.println(num2);
+
+        String result1 = "Today is Monday"; // not "true" or "false" ===> false
+
+        boolean a2 = Boolean.parseBoolean(result1);
+
+        System.out.println(!a2);
+
+        String result2 = "FaLsE";
+
+
+    }
+}
---
+++ src/day29_Wrapper_Array_List/Value_Of.java
@@ -0,0 +1,4 @@
+package day29_Wrapper_Array_List;
+
+public class Value_Of {
+}
