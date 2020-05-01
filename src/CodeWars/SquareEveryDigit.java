5196625d8856f4e7006e5fb7060335a018e71707
bhmeral <bahameral@gmail.com>
2020-05-01 11:48:52 -0700

    commit

--- src/CodeWars/SquareEveryDigit.java
+++ src/CodeWars/SquareEveryDigit.java
@@ -1,6 +1,6 @@
 package CodeWars;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 
 public class SquareEveryDigit {
     public int squareDigits(int n){
--- src/Day32_Predicate/CollectionsMethod.java
+++ src/Day32_Predicate/CollectionsMethod.java
@@ -1,5 +1,6 @@
 package Day32_Predicate;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,8 +8,31 @@ import java.util.Collections;
 public class CollectionsMethod {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-
+        // max min
         System.out.println(Collections.max(list));
         System.out.println(Collections.min(list));
+
+
+        ArrayList<Integer> list1  = new ArrayList<>();
+        //addAll
+        list1.addAll(Arrays.asList(1,2,2,4,1,2,3,4,5));
+
+        //replaceAll
+        Collections.swap(list1,0,4);
+        System.out.println(list1);
+        Collections.replaceAll(list1,1,10);
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Omer","Omer","Omer","Sha","Fatih","Mary"));
+
+        /*
+        names.set(0,"Irina");
+        names.set(1,"Irina");
+        names.set(2,"Irina");
+        */
+
+        Collections.replaceAll(names,"Omer","Irina");
+
+        System.out.println(names);
     }
 }
